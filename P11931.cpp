909c61b30a897c7f4559844e2af732c59b3fe958
static <kmc7468@naver.com>
2020-05-19 14:58:24 +0900

    쉬워 보이지만 어려운 문제 안 푼 문제 풀이

---
+++ P11931.cpp
@@ -0,0 +1,27 @@
+#include <algorithm>
+#include <functional>
+#include <ios>
+#include <iostream>
+#include <vector>
+
+int main() {
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+
+	int n;
+	std::cin >> n;
+
+	std::vector<int> array(n);
+	for (int& element : array) {
+		std::cin >> element;
+	}
+	std::sort(array.begin(), array.end(), std::greater<int>());
+
+	int prev = 0;
+	for (const int element : array) {
+		if (prev != element) {
+			std::cout << element << '\n';
+			prev = element;
+		}
+	}
+}
