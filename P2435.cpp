081b74eade3d129a53a3ad6bc53463cd32977022
static <kmc7468@naver.com>
2020-05-20 10:12:27 +0900

    2435번 문제 풀이 개선

--- P2435.cpp
+++ P2435.cpp
@@ -1,22 +1,26 @@
 #include <algorithm>
 #include <iostream>
+#include <limits>
 
-int g_Temps[100];
-int g_Sums[100];
+int g_Sums[101];
 
 int main() {
 	int n, k;
 	std::cin >> n >> k;
 
 	for (int i = 0; i < n; ++i) {
-		std::cin >> g_Temps[i];
-		if (i < k) {
-			g_Sums[0] += g_Temps[i];
+		int temp;
+		std::cin >> temp;
+		g_Sums[i + 1] = g_Sums[i] + temp;
+	}
+
+	int max = std::numeric_limits<int>::min();
+	for (int i = 0; i < n - k + 1; ++i) {
+		const int sum = g_Sums[i + k] - g_Sums[i];
+		if (max < sum) {
+			max = sum;
 		}
 	}
 
-	for (int i = 1; i < n - k + 1; ++i) {
-		g_Sums[i] = g_Sums[i - 1] - g_Temps[i - 1] + g_Temps[i + k - 1];
-	}
-	std::cout << *std::max_element(g_Sums, g_Sums + n - k + 1);
+	std::cout << max;
 }
