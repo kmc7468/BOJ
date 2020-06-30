305b8961dc423eae41542232691a85614c4089f6
static <kmc7468@naver.com>
2020-07-01 00:01:58 +0900

    2560번 문제 풀이 개선

--- P2560.c
+++ P2560.c
@@ -1,4 +1,3 @@
-#define R(a)((a)%1000+1000)%1000
+#define R(a,b)((a)%b+b)%b
 #define s p[i-1]+p[i-a]
-#define t s-p[i-b]
-p[1000001],i;main(a,b,d,n){scanf("%d%d%d%d",&a,&b,&d,&n);for(;i<=n;++i)p[i]=R(i<a?1:(i<b?s:t-(i==d)));printf("%d",p[n]);}
+p[1000001],i;main(a,b,d,n){scanf("%d%d%d%d",&a,&b,&d,&n);for(;i<=n;++i)p[i]=R(i<a?1:s-(i<b?0:p[i-b]+(i==d)),1000);printf("%d",p[n]);}
