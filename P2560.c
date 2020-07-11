232afade91a62410790069801ec0546a9461a248
static <kmc7468@naver.com>
2020-07-12 00:35:45 +0900

    2560번 문제 풀이 개선

--- P2560.c
+++ P2560.c
@@ -1,1 +1,1 @@
-p[1000001],i;main(a,b,d,n){scanf("%d%d%d%d",&a,&b,&d,&n);for(;i<=n;++i)p[i]=(i<a?1:p[i-1]+p[i-a]-(i<b?0:p[i-b]+(i==d))+1000)%1000;printf("%d",p[n]);}
+p['   '],i;main(a,b,d,n){scanf("%d%d%d%d",&a,&b,&d,&n);for(;i<=n;++i)p[i]=(i<a?1:p[i-1]+p[i-a]-(i<b?0:p[i-b]+(i==d))+1000)%1000;printf("%d",p[n]);}
